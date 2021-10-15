6ca15bd7633d1d9cc86e1d9de92a62c26a0dfecd
chikashi.toyoshima <chikashi.toyoshima@accenture.com>
2021-10-15 23:47:44 +0900

    Initialize Spring

---
+++ src/test/java/com/toyopilgrim/pubsubexample/EntryPointTests.java
@@ -0,0 +1,13 @@
+package com.toyopilgrim.pubsubexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryPointTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
